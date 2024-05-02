ad86ef82f0dd3af2ed1caf2c61bf87dcebc2bd78
synuo <127924043+synuo@users.noreply.github.com>
2024-05-02 16:29:55 +0900

    Update userinfo.dart

--- lib/userinfo.dart
+++ lib/userinfo.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:practice/home.dart';
+import 'package:practice/login.dart';
 
 // 사용자 기본 정보 입력 (이름, 학번, 닉네임, 전공, 비번)
 // 전공1, 전공2, 프로필이미지, 자기소개는 선택사항
@@ -40,6 +41,9 @@ class _UserInfoPageState extends State<UserInfoPage> {
     _fetchMajors();  //전공 데이터 가져옴
   }
 
+  // 전공 이름과 ID를 매핑하는 맵
+  Map<String, int> _majorIdMap = {};
+
   //db로부터 전공 리스트 가져옴
   Future<void> _fetchMajors() async {
     try {
@@ -47,15 +51,30 @@ class _UserInfoPageState extends State<UserInfoPage> {
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
         final List<Map<String, dynamic>> majors = data.cast<Map<String, dynamic>>();
+        _majors = majors.map((major) => major['major_name'] as String).toList();
+        _majors.insert(0, ''); // 맨 앞에 널값 추가
         setState(() {
-          _majors = majors.map((major) => major['name'] as String).toList();
+          _selectedMajor1 = _majors.isNotEmpty ? _majors.first : ''; // 여기서 빈 문자열로 초기화
+          _selectedMajor2 = _majors.isNotEmpty ? _majors.first : '';
+          _selectedMajor3 = _majors.isNotEmpty ? _majors.first : '';
         });
+        // _majors 리스트와 majors 데이터를 동일한 인덱스를 사용하여 전공 이름과 ID를 매핑
+        _majorIdMap = Map.fromIterable(majors,
+            key: (major) => major['major_name'] as String,
+            value: (major) => major['major_id'] as int);
+        print('_majors : $_majors');
+        print('_majoridMap : $_majorIdMap');
       } else {
         throw Exception('Failed to fetch majors');
       }
     } catch (e) {
       print('Error fetching majors: $e');
     }
+  }
+
+  // dropbox에서 선택된 전공 이름에 해당하는 ID를 찾는 함수
+  int? findMajorId(String majorName) {
+    return _majorIdMap[majorName];
   }
 
   @override
@@ -105,7 +124,7 @@ class _UserInfoPageState extends State<UserInfoPage> {
                   if (value.length != 7) {
                     return '학번은 7자리여야 합니다.';
                   }
-                  checkDuplicateStudentId(value); // 학번 중복 체크
+                  //checkDuplicateStudentId(value); // 학번 중복 체크
                   return null;
                 },
               ),
@@ -126,7 +145,7 @@ class _UserInfoPageState extends State<UserInfoPage> {
                   if (value.length > 10) {
                     return '닉네임은 10자 이하여야 합니다.';
                   }
-                  checkDuplicateNickname(value);
+                  //checkDuplicateNickname(value);
                   return null;
                 },
               ),
@@ -170,11 +189,16 @@ class _UserInfoPageState extends State<UserInfoPage> {
               SizedBox(height: 10.0,),
               // 전공 선택 필드
               DropdownButtonFormField<String>(
-                value: _selectedMajor1,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedMajor1 = newValue!;
-                  });
+                  value: _selectedMajor1.isNotEmpty
+                      ? _selectedMajor1
+                      : _majors.isNotEmpty
+                      ? _majors.first
+                      : '', // 초기 값 설정 및 유효성 검사
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedMajor1 = newValue!;
+                    });
+                    print('전공1id : ${findMajorId(_selectedMajor1)}');
                 },
                 decoration: InputDecoration(
                   labelText: '전공',
@@ -255,10 +279,15 @@ class _UserInfoPageState extends State<UserInfoPage> {
                   if (_formKey.currentState != null &&
                       _formKey.currentState!.validate()) {  //회원정보가 모두 성공적으로 입력된 경우
                     // 입력된 데이터 출력 (확인용)
+                    print('이메일:${widget.email}');
                     print('이름: ${_nameController.text}');
+                    print('학번 : ${_studentnumController.text}');
                     print('닉네임: ${_nicknameController.text}');
                     print('비밀번호: ${_passwordController.text}');
-                    print('전공: $_selectedMajor1');
+                    print('전공1: $_selectedMajor1');
+                    print('전공2: $_selectedMajor2');
+                    print('전공3: $_selectedMajor3');
+                    print('프로필이미지: ${_profileImageController.text}');
                     print('자기소개: $_introduction');
                     _registerUser();  // 회원 정보 저장 (=가입)
                   }
@@ -281,69 +310,84 @@ class _UserInfoPageState extends State<UserInfoPage> {
     super.dispose();
   }
 
-  Future <bool> checkDuplicateNickname(String nickname) async {
-    final url = Uri.parse('http://localhost:3000/signup');
-    final response = await http.post(
-      url,
-      body: jsonEncode({
-        'nickname': nickname,
-      }),
-      headers: {'Content-Type': 'application/json'},
-    );
-
-    if (response.statusCode == 200) {
-      print("중복된 닉네임!");
-      _isNicknameAvailable = false;
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('알림'),
-          content: Text('이미 존재하는 닉네임입니다! 새로운 닉네임을 입력해주세요.'),
-          actions: [
-            ElevatedButton(
-              onPressed: () {Navigator.pop(context);} , // 다이얼로그 닫기
-              child: Text('확인'),
-            )
-          ],
-        ),
+  Future<bool> checkDuplicateNickname(String nickname) async {
+    try {
+      final url = Uri.parse('http://localhost:3000/signup');
+      final response = await http.post(
+        url,
+        body: jsonEncode({
+          'nickname': nickname,
+        }),
+        headers: {'Content-Type': 'application/json'},
       );
+
+      if (response.statusCode == 200) {
+        print("중복된 닉네임!");
+        _isNicknameAvailable = false;
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('알림'),
+            content: Text('이미 존재하는 닉네임입니다! 새로운 닉네임을 입력해주세요.'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('확인'),
+              )
+            ],
+          ),
+        );
+        return false;
+      } else {
+        print("사용 가능한 닉네임입니다.");
+        return true;
+      }
+    } catch (e) {
+      print('오류 발생: $e');
       return false;
-    } else {
-      //throw Exception('Failed to check duplicate');
-      print("사용 가능한 닉네임입니다.");
-      return true;
     }
   }
 
-  Future <bool> checkDuplicateStudentId(String studentnum) async {
+  Future<bool> checkDuplicateStudentId(String studentnum) async {
     final url = Uri.parse('http://localhost:3000/signup');
-    final response = await http.post(
-      url,
-      body: jsonEncode({
-        'studentnum': studentnum,
-      }),
-      headers: {'Content-Type': 'application/json'},
-    );
 
-    if (response.statusCode == 200) {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('알림'),
-          content: Text('이미 존재하는 학번입니다! 새로운 학번을 입력해주세요.'),
-          actions: [
-            ElevatedButton(
-              onPressed: () {Navigator.pop(context);} , // 다이얼로그 닫기
-              child: Text('확인'),
-            )
-          ],
-        ),
+    try {
+      final response = await http.post(
+        url,
+        body: jsonEncode({
+          'student_id': studentnum,
+        }),
+        headers: {'Content-Type': 'application/json'},
       );
+
+      if (response.statusCode == 200) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('알림'),
+            content: Text('이미 존재하는 학번입니다! 새로운 학번을 입력해주세요.'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('확인'),
+              )
+            ],
+          ),
+        );
+        _isStudentIdAvailable = false;
+        return false;
+      } else {
+        print("사용 가능한 학번입니다.");
+        _isStudentIdAvailable = true;
+        return true;
+      }
+    } catch (e) {
+      print('오류 발생: $e');
       return false;
-    } else {
-      //throw Exception('Failed to check duplicate');
-      print("사용 가능한 학번입니다.");
-      return true;
     }
   }
 
@@ -353,23 +397,13 @@ class _UserInfoPageState extends State<UserInfoPage> {
     final String nickname = _nicknameController.text;  // 닉네임 저장
     final String password = _passwordController.text;  // 비밀번호 저장
     final String studentnum = _studentnumController.text;  //학번 저장
-    //final String major1 = _selectedMajor1;               // 전공1 저장
-    //final String major2 = _selectedMajor2 ?? '';             // 전공2. null이면 빈 문자열로.
-    //final String major3 = _selectedMajor3 ?? '';             //전공3
-    final int majorId1 = _majors
-        .cast<Map<String, dynamic>>() // 각 요소를 맵으로 캐스팅
-        .firstWhere((element) => element['name'] == _selectedMajor1)['id'];
-    final int majorId2 = _majors
-        .cast<Map<String, dynamic>>() // 각 요소를 맵으로 캐스팅
-        .firstWhere((element) => element['name'] == _selectedMajor2)['id'];
-    final int majorId3 = _majors
-        .cast<Map<String, dynamic>>() // 각 요소를 맵으로 캐스팅
-        .firstWhere((element) => element['name'] == _selectedMajor3)['id'];
+    final int? majorId1 = findMajorId(_selectedMajor1);
+    final int? majorId2 = findMajorId(_selectedMajor2);
+    final int? majorId3 = findMajorId(_selectedMajor3);
     final String profileImage = _profileImageController.text; //프로필 이미지 저장
     final String introduction = _introduction;         // 자기소개 저장
 
     final Uri uri = Uri.parse('http://localhost:3000/signup');
-
 
     try {    // 회원 등록을 위한 API 호출 등의 로직 추가
       final response = await http.post(    // 서버로 회원 가입 요청을 보냄
@@ -382,7 +416,7 @@ class _UserInfoPageState extends State<UserInfoPage> {
           'name' : name,
           'nickname': nickname,
           'password': password,
-          'studentnum' : studentnum,
+          'student_id' : studentnum,
           'major1': majorId1,  //이름 대신 id 전달
           'major2': majorId2,
           'major3': majorId3,
@@ -393,10 +427,41 @@ class _UserInfoPageState extends State<UserInfoPage> {
 
       if (response.statusCode == 201) {  // 회원 가입 성공
         print('회원 가입 성공!');
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => HomePage()), //홈 화면으로 이동
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('알림'),
+            content: Text('회원 가입이 완료되었습니다! 다시 로그인을 진행해주세요.'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // 팝업 닫기
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()), //홈 화면으로 이동
+                  );
+                },
+                child: Text('확인'),
+              )
+            ],
+          ),
         );
+        print('화면전환 : userinfo -> login');
+      } else if (response.statusCode == 400) {
+        final Map<String, dynamic> responseData = json.decode(response.body);
+        if (responseData.containsKey('error')) {
+          final String error = responseData['error'];
+          if (error.contains('nickname')) {
+            setState(() {
+              _isNicknameAvailable = false;
+            });
+          }
+          if (error.contains('student_id')) {
+            setState(() {
+              _isStudentIdAvailable = false;
+            });
+          }
+        }
       } else {  // 회원 가입 실패
         print('회원 가입 실패: ${response.body}');
       }
