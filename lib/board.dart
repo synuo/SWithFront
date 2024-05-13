32beb61709287332a794979dc6ecf3deb862dc7b
solux <leehn45@naver.com>
2024-05-13 16:42:42 +0900

    mod / 아래로 당기면 게시글 새로고침 되는 기능 추가 (?)
    Test 못해봄 web에서는 안됨

--- lib/board.dart
+++ lib/board.dart
@@ -61,7 +61,6 @@ class _BoardScreenState extends State<BoardScreen> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,57 +88,60 @@ class _BoardScreenState extends State<BoardScreen> {
         ],
       ),
 
-      body: posts.isEmpty
-          ? Center(
-        child: CircularProgressIndicator(),
-      )
-          : ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: (BuildContext context, int index) {
-          final Post post = posts[index];
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PostDetailScreen(post_id: post.post_id),
-                ),
-              );
-            },
-            child: Container(
-              height: 136,
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: const Color(0xFFE0E0E0)),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          post.title,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          "${post.category} · ${post.progress} · ${post.view_count}",
-                          style: Theme.of(context).textTheme.caption,
-                        ),
-                      ],
-                    ),
+      body: RefreshIndicator(
+        onRefresh: () => fetchPosts(),
+        child: posts.isEmpty
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (BuildContext context, int index) {
+            final Post post = posts[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PostDetailScreen(post_id: post.post_id),
                   ),
-                ],
+                );
+              },
+              child: Container(
+                height: 136,
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFFE0E0E0)),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            post.title,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            "${post.category} · ${post.progress} · ${post.view_count}",
+                            style: Theme.of(context).textTheme.caption,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
 
       bottomNavigationBar: CustomBottomNavigationBar(
