e15f8dd2f691c24b774aa4b2c3b712ba031b017a
synuo <127924043+synuo@users.noreply.github.com>
2024-05-15 22:44:48 +0900

    Update findpw.dart

--- lib/findpw.dart
+++ lib/findpw.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:practice/main.dart';
 
 class FindPassword extends StatefulWidget {
   const FindPassword({super.key});
