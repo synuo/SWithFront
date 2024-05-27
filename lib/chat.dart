deba648d0419f398238f222a872d18efac488cfc
ha9eun <ha9eunsoo@gmail.com>
2024-05-27 21:49:18 +0900

    Feat/채팅방 목록에서 미리보기 추가중

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,14 +1,11 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-import 'board.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'common_object.dart';
-import 'common_widgets.dart';
-import 'home.dart';
-import 'mypage.dart';
 import 'chat_room_screen.dart';
+import 'package:intl/intl.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -18,36 +15,72 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   List<Map<String, dynamic>> chatRooms = [];
   User? loggedInUser;
+  late IO.Socket socket;
 
   @override
   void initState() {
     super.initState();
     loggedInUser = Provider.of<UserProvider>(context, listen: false).loggedInUser;
     fetchData();
+    initializeSocket();
   }
 
   Future<void> fetchData() async {
     final response = await http.post(
       Uri.parse('http://localhost:3000/getchatrooms'),
       body: {'userId': loggedInUser?.user_id.toString()},
-      // 유저 아이디를 전송
     );
 
     if (response.statusCode == 200) {
-      // 정상적으로 응답을 받았을 때
       final data = jsonDecode(response.body);
       setState(() {
-        chatRooms = List<Map<String, dynamic>>.from(data['data']);//room_id, study_name
+        chatRooms = List<Map<String, dynamic>>.from(data['data']);
       });
     } else {
-      // 오류가 발생했을 때
       print('Failed to load data');
+    }
+  }
+
+  void initializeSocket() {
+    socket = IO.io('http://localhost:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+
+    socket.connect();
+    socket.onConnect((_) {
+      print('connected');
+    });
+
+    socket.on('newMessage', (data) {
+      setState(() {
+        final updatedRoom = chatRooms.firstWhere((room) => room['room_id'] == data['room_id']);
+        updatedRoom['last_message'] = data['last_message'];
+        updatedRoom['last_message_time'] = data['last_message_time'];
+        chatRooms.sort((a, b) {
+          final timeA = a['last_message_time'] != null ? DateTime.parse(a['last_message_time']) : DateTime.fromMillisecondsSinceEpoch(0);
+          final timeB = b['last_message_time'] != null ? DateTime.parse(b['last_message_time']) : DateTime.fromMillisecondsSinceEpoch(0);
+          return timeB.compareTo(timeA);
+        });
+      });
+    });
+
+    socket.onDisconnect((_) => print('disconnected'));
+  }
+
+  String formatMessageTimestamp(String timestamp) {
+    final now = DateTime.now();
+    final messageTime = DateTime.parse(timestamp).toLocal();
+
+    if (now.year == messageTime.year && now.month == messageTime.month && now.day == messageTime.day) {
+      return DateFormat('HH:mm').format(messageTime);
+    } else {
+      return DateFormat('yyyy-MM-dd').format(messageTime);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    //int _currentIndex = 2;
     return Scaffold(
       appBar: AppBar(
         title: Text('채팅'),
@@ -57,9 +90,25 @@ class _ChatScreenState extends State<ChatScreen> {
           : ListView.builder(
         itemCount: chatRooms.length,
         itemBuilder: (context, index) {
+          final lastMessage = chatRooms[index]['last_message'];
+          final lastMessageTime = chatRooms[index]['last_message_time'];
           return Card(
             child: ListTile(
               title: Text(chatRooms[index]['study_name']),
+              subtitle: lastMessage != null && lastMessageTime != null
+                  ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(lastMessage, overflow: TextOverflow.ellipsis),
+                  ),
+                  Text(
+                    formatMessageTimestamp(lastMessageTime),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              )
+                  : null,
               onTap: () {
                 Navigator.push(
                   context,
@@ -75,29 +124,12 @@ class _ChatScreenState extends State<ChatScreen> {
           );
         },
       ),
-      /*
-      bottomNavigationBar: CustomBottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-          switch (index) {
-            case 0: // 홈 아이콘
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
-              break;
-            case 1: // 게시판 아이콘
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => BoardScreen()));
-              break;
-            case 2: // 채팅 아이콘
-
-              break;
-            case 3: // 마이페이지 아이콘
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyPage()));
-              break;
-          }
-        },
-      ),*/
     );
+  }
+
+  @override
+  void dispose() {
+    socket.dispose();
+    super.dispose();
   }
 }
--- lib/chat_room_screen.dart
+++ lib/chat_room_screen.dart
@@ -27,12 +27,12 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
   @override
   void initState() {
     super.initState();
-    loggedInUser = Provider.of<UserProvider>(context, listen: false).loggedInUser;
-    _initializeSocket();
-    _scrollController.addListener(_scrollListener);
+    loggedInUser = Provider.of<UserProvider>(context, listen: false).loggedInUser; //현재 로그인된 유저 정보
+    _initializeSocket(); //소켓 초기화
+    _scrollController.addListener(_scrollListener); //스크롤할 때마다 _scrollListener 호출
   }
 
-  void _initializeSocket() {
+  void _initializeSocket() { //소켓 초기화
     socket = IO.io('http://localhost:3000', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
@@ -41,64 +41,67 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
     socket.connect();
     socket.onConnect((_) {
       print('connected');
-      socket.emit('joinRoom', widget.roomId);
-      _fetchChatHistory();
+      socket.emit('joinRoom', widget.roomId); //방 입장
+      _fetchChatHistory(); //채팅 내역 불러오기
     });
 
-    socket.on('chatMessage', (data) {
-      setState(() {
-        messages.insert(0, data);
+    socket.on('chatMessage', (data) { // 실시간으로 새로운 메시지 수신. 서버에 새로운 메시지가 도착하면 이 콜백 함수 호출
+      setState(() { // 상태 업데이트
+        messages.insert(0, data); // 리스트 맨앞에 새로운 메시지 삽입
       });
+      _scrollController.animateTo( //최신 메시지 볼 수 있도록 맨 위로 스크롤
+        0.0,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
     });
 
-    socket.onDisconnect((_) => print('disconnected'));
-  }
-
-  Future<void> _fetchChatHistory() async {
-    if (_isLoadingMore || !_hasMoreMessages) return;
-
-    _isLoadingMore = true;
-    final lastMessageTime = messages.isNotEmpty ? messages.last['chat_time'] : null;
-
-    socket.emit('getChatHistory', {
-      'roomId': widget.roomId,
-      'limit': _messageLimit,
-      'lastMessageTime': lastMessageTime,
-    });
-
-    socket.once('chatHistory', (data) {
+    socket.on('chatHistory', (data) { // 과거의 채팅기록 수신._fetchChatHistory()에서 요청하고,  서버에서 과거 채팅 기록 데이터를 보내주면 이 콜백 함수가 호출됨
       final newMessages = List<Map<String, dynamic>>.from(data['data']);
       setState(() {
         if (newMessages.length < _messageLimit) {
           _hasMoreMessages = false;
         }
-        messages.addAll(newMessages.reversed);
+        messages.addAll(newMessages.reversed); // 시간 순서대로 추가
       });
       _isLoadingMore = false;
+    });
+
+    socket.onDisconnect((_) => print('disconnected'));
+  }
+
+  Future<void> _fetchChatHistory() async { // 서버에 과거 메시지 요청
+    if (_isLoadingMore || !_hasMoreMessages) return; //로딩중이거나 더이상 불러올 메시지가 없다면 바로 반환
+
+    _isLoadingMore = true; //현재 메시지 불러오는 중
+    final lastMessageTime = messages.isNotEmpty ? messages.last['chat_time'] : null; // 불러온 메시지 중 가장 과거 메시지의 시간
+
+    socket.emit('getChatHistory', {
+      'roomId': widget.roomId, // 방 ID
+      'limit': _messageLimit, // 20
+      'lastMessageTime': lastMessageTime, // 그 이후 시간 메시지 요청
     });
   }
 
   void _scrollListener() {
-    if (_scrollController.position.atEdge && _scrollController.position.pixels != 0) {
-      _fetchChatHistory();
+    if (_scrollController.position.atEdge && _scrollController.position.pixels != 0) { // 스크롤이 리스트의 끝에 있을 때
+      _fetchChatHistory(); //서버에 과거 메시지 요청
     }
   }
 
-  void _sendMessage() {
+  void _sendMessage() { // 새로운 메시지 전송
     if (_controller.text.isNotEmpty) {
-
       final now = DateTime.now().toIso8601String();
       final message = {
         'roomId': widget.roomId,
         'sender_id': loggedInUser?.user_id,
-        'nickname': loggedInUser?.nickname,
         'content': _controller.text,
         'chat_time': now,
       };
-      socket.emit('chatMessage', message);
-      _controller.clear();
-      _scrollController.animateTo(
-        0.0,
+      socket.emit('chatMessage', message); //서버로 메시지 전송
+      _controller.clear(); //입력창 초기화
+      _scrollController.animateTo( //맨 위로 스크롤하여 최신 메시지 표시
+        0.0, //스크롤 위치를 0으로(맨위)
         duration: Duration(milliseconds: 300),
         curve: Curves.easeOut,
       );
@@ -106,13 +109,11 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
   }
 
   String _formatTimestamp(String timestamp) {
-
     final dateTime = DateTime.parse(timestamp).toLocal();
     return DateFormat('HH:mm').format(dateTime);
   }
 
   String _formatDate(String timestamp) {
-
     final dateTime = DateTime.parse(timestamp).toLocal();
     return DateFormat('yyyy-MM-dd').format(dateTime);
   }
